e612baa7b09f9102f186a4784b05793745cd89f3
anth64 <anth64@protonmail.com>
2026-03-13 07:59:22 +0100

    build(bsd): fix path nesting

--- bmake.mk
+++ bmake.mk
@@ -9,64 +9,59 @@ CLIENT_FULL_BIN = ${CLIENT_BIN}
 SERVER_FULL_BIN = ${SERVER_BIN}
 .endif
 
-LDFLAGS_PLAT =
-CFLAGS_PLAT  =
+LDFLAGS_PLAT = -L/usr/local/lib
+CFLAGS_PLAT  = -I/usr/local/include
 CFLAGS_BASE  = -Wall -Wpedantic -I${.CURDIR}/${INC_DIR} -std=c99 ${CFLAGS_PLAT}
 
 ALL_CLIENT_SRCS = ${CLIENT_SRCS} ${ENGINE_SRCS}
 ALL_SERVER_SRCS = ${SERVER_SRCS} ${ENGINE_SRCS}
 
-# All unique sources combined (no duplicates)
 ALL_SRCS = ${CLIENT_SRCS} ${SERVER_SRCS} ${ENGINE_SRCS}
 
-CLIENT_DEBUG_OBJS   = ${ALL_CLIENT_SRCS:S/.c$/.o/:S/^/obj\/debug\//}
-CLIENT_RELEASE_OBJS = ${ALL_CLIENT_SRCS:S/.c$/.o/:S/^/obj\/release\//}
-SERVER_DEBUG_OBJS   = ${ALL_SERVER_SRCS:S/.c$/.o/:S/^/obj\/debug\//}
-SERVER_RELEASE_OBJS = ${ALL_SERVER_SRCS:S/.c$/.o/:S/^/obj\/release\//}
+CLIENT_DEBUG_OBJS   = ${ALL_CLIENT_SRCS:S/.c$/.o/:S|^|${.CURDIR}/obj/debug/|}
+CLIENT_RELEASE_OBJS = ${ALL_CLIENT_SRCS:S/.c$/.o/:S|^|${.CURDIR}/obj/release/|}
+SERVER_DEBUG_OBJS   = ${ALL_SERVER_SRCS:S/.c$/.o/:S|^|${.CURDIR}/obj/debug/|}
+SERVER_RELEASE_OBJS = ${ALL_SERVER_SRCS:S/.c$/.o/:S|^|${.CURDIR}/obj/release/|}
 
 PREFIX ?= /usr/local
 
 .PHONY: all debug release client server install uninstall clean
 all: debug
 
-debug: ${BIN_DIR}/debug/${CLIENT_FULL_BIN} ${BIN_DIR}/debug/${SERVER_FULL_BIN}
-release: ${BIN_DIR}/release/${CLIENT_FULL_BIN} ${BIN_DIR}/release/${SERVER_FULL_BIN}
-client: ${BIN_DIR}/debug/${CLIENT_FULL_BIN}
-server: ${BIN_DIR}/debug/${SERVER_FULL_BIN}
+debug: ${.CURDIR}/${BIN_DIR}/debug/${CLIENT_FULL_BIN} ${.CURDIR}/${BIN_DIR}/debug/${SERVER_FULL_BIN}
+release: ${.CURDIR}/${BIN_DIR}/release/${CLIENT_FULL_BIN} ${.CURDIR}/${BIN_DIR}/release/${SERVER_FULL_BIN}
+client: ${.CURDIR}/${BIN_DIR}/debug/${CLIENT_FULL_BIN}
+server: ${.CURDIR}/${BIN_DIR}/debug/${SERVER_FULL_BIN}
 
-${BIN_DIR}/debug/${CLIENT_FULL_BIN}: ${CLIENT_DEBUG_OBJS}
+${.CURDIR}/${BIN_DIR}/debug/${CLIENT_FULL_BIN}: ${CLIENT_DEBUG_OBJS}
 	@mkdir -p ${.TARGET:H}
 	${CC} -o ${.TARGET} ${.ALLSRC} ${LDFLAGS_CLIENT} ${LDFLAGS_PLAT}
 
-${BIN_DIR}/debug/${SERVER_FULL_BIN}: ${SERVER_DEBUG_OBJS}
+${.CURDIR}/${BIN_DIR}/debug/${SERVER_FULL_BIN}: ${SERVER_DEBUG_OBJS}
 	@mkdir -p ${.TARGET:H}
 	${CC} -o ${.TARGET} ${.ALLSRC} ${LDFLAGS_SERVER} ${LDFLAGS_PLAT}
 
-${BIN_DIR}/release/${CLIENT_FULL_BIN}: ${CLIENT_RELEASE_OBJS}
+${.CURDIR}/${BIN_DIR}/release/${CLIENT_FULL_BIN}: ${CLIENT_RELEASE_OBJS}
 	@mkdir -p ${.TARGET:H}
 	${CC} -s -o ${.TARGET} ${.ALLSRC} ${LDFLAGS_CLIENT} ${LDFLAGS_PLAT}
 
-${BIN_DIR}/release/${SERVER_FULL_BIN}: ${SERVER_RELEASE_OBJS}
+${.CURDIR}/${BIN_DIR}/release/${SERVER_FULL_BIN}: ${SERVER_RELEASE_OBJS}
 	@mkdir -p ${.TARGET:H}
 	${CC} -s -o ${.TARGET} ${.ALLSRC} ${LDFLAGS_SERVER} ${LDFLAGS_PLAT}
 
 .for _src in ${ALL_SRCS}
-_obj_base = ${_src:S/.c$/.o/}
-obj/debug/${_obj_base}: ${_src}
+${.CURDIR}/obj/debug/${_src:S/.c$/.o/}: ${.CURDIR}/${_src}
 	@mkdir -p ${.TARGET:H}
-	${CC} ${CFLAGS_BASE} -g -O0 -MMD -MP -c ${.ALLSRC} -o ${.TARGET}
-obj/release/${_obj_base}: ${_src}
+	${CC} ${CFLAGS_BASE} -g -O0 -MMD -MP -MT ${.TARGET} -MF ${.TARGET:S/.o$/.d/} -c ${.ALLSRC} -o ${.TARGET}
+${.CURDIR}/obj/release/${_src:S/.c$/.o/}: ${.CURDIR}/${_src}
 	@mkdir -p ${.TARGET:H}
-	${CC} ${CFLAGS_BASE} -O2 -MMD -MP -c ${.ALLSRC} -o ${.TARGET}
+	${CC} ${CFLAGS_BASE} -O2 -MMD -MP -MT ${.TARGET} -MF ${.TARGET:S/.o$/.d/} -c ${.ALLSRC} -o ${.TARGET}
 .endfor
-
-.-include "obj/debug/*.d"
-.-include "obj/release/*.d"
 
 install:
 	install -d ${PREFIX}/bin
-	install -m 755 ${BIN_DIR}/release/${CLIENT_FULL_BIN} ${PREFIX}/bin/${CLIENT_FULL_BIN}
-	install -m 755 ${BIN_DIR}/release/${SERVER_FULL_BIN} ${PREFIX}/bin/${SERVER_FULL_BIN}
+	install -m 755 ${.CURDIR}/${BIN_DIR}/release/${CLIENT_FULL_BIN} ${PREFIX}/bin/${CLIENT_FULL_BIN}
+	install -m 755 ${.CURDIR}/${BIN_DIR}/release/${SERVER_FULL_BIN} ${PREFIX}/bin/${SERVER_FULL_BIN}
 
 uninstall:
 	rm -f ${PREFIX}/bin/${CLIENT_FULL_BIN}
